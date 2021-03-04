fcf4abf78929dccb868ef0d182be4a037b93233e
Steve Bishop <airbish@gmail.com>
2021-03-04 17:10:43 -0500

    change to hard code vars

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 module "s3-webapp" {
   source  = "app.terraform.io/bishorg/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
+  name   = "bish-private-module-test" 
+  region = "us-east-1"
+  prefix =  "dev"
   version = "1.0.0"
 }
